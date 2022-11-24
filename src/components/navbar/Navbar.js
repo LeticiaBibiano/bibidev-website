e4186b24133ec412735f7f4e3f903d7fb8414c31
LeticiaBibiano <leticiabibianob@gmail.com>
2022-11-23 23:02:39 -0300

    first commit: add packages, styled-components, react-router-som and react-icons

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,10 @@
+function Navbar () {
+    return (
+        <menu>
+            <img src="logo.png" alt="Logo Bibi Dev"/>
+            <h1>Bibi.dev</h1>
+        </menu>
+    )
+}
+
+export default Navbar
